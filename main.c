f43de72ea704571bc7abd3f1592ccb82b5ab51ab
mubiao1 <15513822207@163.com>
2017-09-16 15:52:40 +0800

    添加main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+int main(int argc, char *argv[])
+{
+    printf("hello world!\n");
+    return 0;
+}
+
